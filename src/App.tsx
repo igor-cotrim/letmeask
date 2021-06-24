14d9ac9862e95dc078893d50ed2ca09e131fecdf
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-06-23 22:12:16 -0300

    criou hook useRoom, riou pagina de admin, criou funcionalidade de like, criou remoção de pergunta e funcionalidade de encerrar sala

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from './pages/AdminRoom';
 
 import {AuthContextProvider} from './contexts/AuthContext'
 
@@ -14,10 +15,13 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/salas/nova" component={NewRoom} />
           <Route path="/salas/:id" component={Room} />
+          <Route path="/admin/salas/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
 }
+
+// --refatorar o codigo
 
 export default App;
